6c8969c2cf752bdc09fbf866e8dabda3bcb98313
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-07-30 15:09:56 +0900

    Update help message

--- src/true.hs
+++ src/true.hs
@@ -4,10 +4,20 @@ import System.Environment
 import System.Exit
 
 usage :: IO ()
-usage = putStrLn "Usage: true [OPTION]"
+usage =
+  putStrLn
+    "Usage: true [OPTION]\n\
+    \       -h, --help        Print help message\n\
+    \       -v, --version     Print version\n\
+    \\n\
+    \hs-coreutils project software written in haskell,\n\
+    \licensed under the terms of the MIT LICENSE.\n\
+    \\n\
+    \URL   :https://github.com/nao1215/hs-coreutils\n\
+    \Author: Naohiro CHIKAMATSU"
 
 version :: IO ()
-version = putStrLn "Haskell true 0.1.0"
+version = putStrLn "version 0.1.0"
 
 main :: IO ()
 main = do
