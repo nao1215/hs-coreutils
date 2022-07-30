8a362acff1acb98ec916b0d3e3ae1a860af1cfee
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-07-30 14:55:11 +0900

    Fixed Warnings in Linter (refactoring)

--- src/true.hs
+++ src/true.hs
@@ -4,24 +4,19 @@ import System.Environment
 import System.Exit
 
 usage :: IO ()
-usage   = putStrLn "Usage: true [-vh]"
+usage = putStrLn "Usage: true [OPTION]"
 
 version :: IO ()
 version = putStrLn "Haskell true 0.1.0"
 
-exit :: IO a
-exit    = exitWith ExitSuccess
-
 main :: IO ()
 main = do
-    args <- getArgs
-    if length args == 0
-    then
-        exit
-    else
-        case head args of
-        "-h"        -> usage   >> exit
-        "--help"    -> usage   >> exit
-        "-v"        -> version >> exit
-        "--version" -> version >> exit
-        _           -> exit
+  args <- getArgs
+  if null args
+    then System.Exit.exitSuccess
+    else case head args of
+      "-h" -> usage >> System.Exit.exitSuccess
+      "--help" -> usage >> System.Exit.exitSuccess
+      "-v" -> version >> System.Exit.exitSuccess
+      "--version" -> version >> System.Exit.exitSuccess
+      _ -> System.Exit.exitSuccess
